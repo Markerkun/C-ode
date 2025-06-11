95b0a584438bc4984d33e1dd234622b253384f23
DEV <DEV@A21PC16>
2025-06-11 18:00:10 +0300

    Add project files.

---
+++ 01_FirstLesson/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_FirstLesson
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
