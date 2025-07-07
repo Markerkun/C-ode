e30fd05d47fb0d08528d8b499066d5aeff54adf4
Mark <markmyskovets9@gmail.com>
2025-07-07 16:01:58 +0300

    Hw

---
+++ 12_EventCallBackFunction/Hw.cs
@@ -0,0 +1,143 @@
+﻿
+namespace _12_EventCallBackFunction
+{
+    
+    //////////////////////////
+    
+    static class StringExtensions
+    {
+        //1
+        public static bool IsPalindrome(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return false;
+            int left = 0;
+            int right = str.Length - 1;
+            while (left < right)
+            {
+                if (str[left] != str[right])
+                {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+        //2
+        public static string Cipher(this string str)
+        {
+            if (string.IsNullOrEmpty(str)) return str;
+            char[] ciphered = new char[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (char.IsLetter(str[i]))
+                {
+                    ciphered[i] = (char)(str[i] - 3);
+                }
+                else
+                {
+                    ciphered[i] = str[i]; 
+                }
+            }
+            return new string(ciphered);
+        }
+    }
+
+    static class ArrayExtensions
+    {
+        //3
+        public static int AmountOfSameChar(this string[] array, char sym)
+        {
+            int count = 0;
+            foreach (string item in array)
+            {
+                foreach (var c in item)
+                {
+                    if (c == sym)
+                    {
+                        count++;
+                    }
+                }
+                
+            }
+            return count;
+        }
+        public static int AmountOfSameInt(this int[] array, int sym)
+        {
+            int count = 0;
+            foreach (int item in array)
+            {
+                    if (item == sym)
+                    {
+                        count++;
+                    }
+                
+            }
+            return count;
+        }
+        public static int AmountOfSameFloat(this float[] array, float sym)
+        {
+            int count = 0;
+            foreach (float item in array)
+            {
+                    if (item == sym)
+                    {
+                        count++;
+                    }
+                
+            }
+            return count;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1
+            Console.WriteLine("Enter a word to check if it's a palindrome:");
+            string input = Console.ReadLine();
+            if (input.IsPalindrome())
+            {
+                Console.WriteLine($"The word '{input}' is a palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"The word '{input}' is not a palindrome.");
+            }
+            ///////////////////
+            //2
+            Console.WriteLine("Enter a string to cipher:");
+            string text = Console.ReadLine();
+            string cipheredText = text.Cipher();
+            Console.WriteLine($"Ciphered text: {cipheredText}");
+            ///////////////////
+            //3
+            int[] intArray = { 1, 2, 3, 4, 5, 1, 2, 1 };
+            Console.WriteLine("Enter an integer to count its occurrences in the array:");
+            int intSym = int.Parse(Console.ReadLine());
+            int intCount = intArray.AmountOfSameInt(intSym);
+            Console.WriteLine($"The integer {intSym} occurs {intCount} times in the array.");
+            Console.WriteLine();
+
+            ///////////////////
+            string[] stringArray = { "apple", "banana", "cherry", "apple" };
+            Console.WriteLine("Enter a character to count its occurrences in the string array:");
+            string charSym = Console.ReadLine();
+            char character = charSym[0];
+            int charCount = stringArray.AmountOfSameChar(character);
+            Console.WriteLine();
+
+            ///////////////////
+            float[] floatArray = { 1.1f, 2.2f, 3.3f, 1.1f, 4.4f };
+            Console.WriteLine("Enter a float to count its occurrences in the float array:");
+            float floatSym = float.Parse(Console.ReadLine());
+            int floatCount = floatArray.AmountOfSameFloat(floatSym);
+            Console.WriteLine($"The float {floatSym} occurs {floatCount} times in the float array.");
+
+
+        }
+
+    }
+}
--- 12_EventCallBackFunction/Program.cs
+++ 12_EventCallBackFunction/Program.cs
@@ -1,102 +1,102 @@
-﻿using System;
+﻿//using System;
 
-namespace _12_EventCallBackFunction
-{
+//namespace _12_EventCallBackFunction
+//{
 
-    class Trader
-    {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-        public double BalanceDollar { get; set; }
-        public double BalanceRobux { get; set; }
-        private double TotalCourse = 42;
-        public Trader(double balanceDollar, double balanceRobux, string name, string surname)
-        {
-            BalanceDollar = balanceDollar;
-            BalanceRobux = balanceRobux;
-            Name = name;
-            Surname = surname;
-        }
-        public void Reaction(double Course)
-        {
-            TotalCourse = Course;
-            if (Course < 40)
-            {
-                Console.WriteLine($"I'm selling all robux");
-                ExchangeToDollar();
-                Console.WriteLine($"I have {Math.Round(BalanceDollar, 2)} dollars and {Math.Round(BalanceRobux, 2)} robux now");
-            }
-            else if (Course > 42)
-            {
-                Console.WriteLine($"I'm selling dollar");
-                ExchangeToRobux();
-                Console.WriteLine($"I have {Math.Round(BalanceDollar, 2)} dollars and {Math.Round(BalanceRobux, 2)} robux now");
-            }
-            else
-            {
-                Console.WriteLine($"I don't care about dollar course");
-            }
-        }
-        public void ExchangeToRobux()
-        { 
-            this.BalanceRobux = this.BalanceDollar * TotalCourse;
-            BalanceDollar = 0;
-        }
-        public void ExchangeToDollar()
-        {
-            this.BalanceDollar = this.BalanceRobux / TotalCourse;
-            BalanceRobux = 0;
-        }
-    }
-
-
+//    class Trader
+//    {
+//        public string Name { get; set; }
+//        public string Surname { get; set; }
+//        public double BalanceDollar { get; set; }
+//        public double BalanceRobux { get; set; }
+//        private double TotalCourse = 42;
+//        public Trader(double balanceDollar, double balanceRobux, string name, string surname)
+//        {
+//            BalanceDollar = balanceDollar;
+//            BalanceRobux = balanceRobux;
+//            Name = name;
+//            Surname = surname;
+//        }
+//        public void Reaction(double Course)
+//        {
+//            TotalCourse = Course;
+//            if (Course < 40)
+//            {
+//                Console.WriteLine($"I'm selling all robux");
+//                ExchangeToDollar();
+//                Console.WriteLine($"I have {Math.Round(BalanceDollar, 2)} dollars and {Math.Round(BalanceRobux, 2)} robux now");
+//            }
+//            else if (Course > 42)
+//            {
+//                Console.WriteLine($"I'm selling dollar");
+//                ExchangeToRobux();
+//                Console.WriteLine($"I have {Math.Round(BalanceDollar, 2)} dollars and {Math.Round(BalanceRobux, 2)} robux now");
+//            }
+//            else
+//            {
+//                Console.WriteLine($"I don't care about dollar course");
+//            }
+//        }
+//        public void ExchangeToRobux()
+//        { 
+//            this.BalanceRobux = this.BalanceDollar * TotalCourse;
+//            BalanceDollar = 0;
+//        }
+//        public void ExchangeToDollar()
+//        {
+//            this.BalanceDollar = this.BalanceRobux / TotalCourse;
+//            BalanceRobux = 0;
+//        }
+//    }
 
 
-    public delegate void ReactDelegate(double course);
-    class Exchange
-    {
-        public double DollarCourse { get; set; } = 42.0;
 
-        public event ReactDelegate Reaction;
 
-        public void ChangeCurrency()
-        {
-            Random random = new Random();
-            DollarCourse = random.Next(30, 50);
-            Console.WriteLine($"Dollar course changed to {DollarCourse}");
-            Reaction?.Invoke(DollarCourse);
-        }
-        public string ToString()
-        {
-            return $"Dollar course: {DollarCourse}";
-        }
-    }
-    internal class Program
-    {
-        static void Main()
-        {
-            List<Trader> traders = new List<Trader>() {
-                new Trader(100,1000, "Mukola", "Oliunuk"),
-                new Trader(50,1500, "Ira", "Oliunuk"),
-                new Trader(200,500, "Nikita", "Oliunuk"),
-                new Trader(100,2000, "Petro", "Oliunuk"),
-                new Trader(10,10000, "Olga", "Oliunuk")
-            };
-            Exchange exchange = new Exchange();
-            foreach (Trader trader in traders)
-            {
-                exchange.Reaction += trader.Reaction;
-            }
-            Console.WriteLine(exchange.ToString());
-            exchange.ChangeCurrency();
-            Console.WriteLine();
-            exchange.ChangeCurrency();
-            Console.WriteLine();
-            exchange.ChangeCurrency();
-            Console.WriteLine();
-            exchange.ChangeCurrency();
-            Console.WriteLine();
+//    public delegate void ReactDelegate(double course);
+//    class Exchange
+//    {
+//        public double DollarCourse { get; set; } = 42.0;
 
-        }
-    }
-}
+//        public event ReactDelegate Reaction;
+
+//        public void ChangeCurrency()
+//        {
+//            Random random = new Random();
+//            DollarCourse = random.Next(30, 50);
+//            Console.WriteLine($"Dollar course changed to {DollarCourse}");
+//            Reaction?.Invoke(DollarCourse);
+//        }
+//        public string ToString()
+//        {
+//            return $"Dollar course: {DollarCourse}";
+//        }
+//    }
+//    internal class Program
+//    {
+//        static void Main()
+//        {
+//            List<Trader> traders = new List<Trader>() {
+//                new Trader(100,1000, "Mukola", "Oliunuk"),
+//                new Trader(50,1500, "Ira", "Oliunuk"),
+//                new Trader(200,500, "Nikita", "Oliunuk"),
+//                new Trader(100,2000, "Petro", "Oliunuk"),
+//                new Trader(10,10000, "Olga", "Oliunuk")
+//            };
+//            Exchange exchange = new Exchange();
+//            foreach (Trader trader in traders)
+//            {
+//                exchange.Reaction += trader.Reaction;
+//            }
+//            Console.WriteLine(exchange.ToString());
+//            exchange.ChangeCurrency();
+//            Console.WriteLine();
+//            exchange.ChangeCurrency();
+//            Console.WriteLine();
+//            exchange.ChangeCurrency();
+//            Console.WriteLine();
+//            exchange.ChangeCurrency();
+//            Console.WriteLine();
+
+//        }
+//    }
+//}
