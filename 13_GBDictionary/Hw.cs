614aa2553c0de86c816a55d39792a38b21520fea
DEV <DEV@A21PC16>
2025-07-07 20:54:54 +0300

    Hw not all

---
+++ 13_GBDictionary/Hw.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace _13_HwDictionary
+{
+    internal class Hw
+    {
+        static void Main(string[] args)
+        {
+            string text = "Ось будинок, який збудував Джек. А це пшениця, яка\r\nу темній коморі зберігається у будинку, який збудував\r\nДжек. А це веселий птах-синиця, який часто краде\r\nпшеницю, яка в темній коморі зберігається у будинку,\r\nякий збудував Джек.";
+            string[] words = text.Split(new char[] { ' ', '!', '?', ',', '.', ':', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> wordCount = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                string lowerWord = word.ToLower();
+                if (wordCount.ContainsKey(lowerWord))
+                {
+                    wordCount[lowerWord]++;
+                }
+                else
+                {
+                    wordCount[lowerWord] = 1;
+                }
+            }
+            Console.WriteLine("Слово - Кількість входжень");
+            foreach (KeyValuePair<string, int> word in wordCount)
+            {
+                Console.WriteLine($"Key : {word.Key,20} Value : {word.Value,50}");
+            }
+        }
+    }
+}
