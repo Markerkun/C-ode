a435e2b0320766ac3b022783f0d3f5c6c46e582c
DEV <DEV@A21PC16>
2025-07-16 20:50:12 +0300

    Regex2

---
+++ 17_Regex/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _16_Regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            string email;
+            Console.WriteLine("Enter your password: ");
+            password = Console.ReadLine();
+            Console.WriteLine("Enter your email: ");
+            email = Console.ReadLine();
+            string emailpattern = @"^[\w\-\.]{4,}\@\w{2,}\.(com)$";
+            string passwordpattern = @"^[\w\-]{6,}$";
+
+        }
+    }
+}
